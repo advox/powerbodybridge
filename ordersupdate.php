bf66c98e0dc428f58f7b7c7e0231fa575e8db94a
michal nowicki <m.nowicki@advoxstudio.com>
2017-05-04 08:05:21 +0200

    init

---
+++ ordersupdate.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'app/Mage.php';
+Varien_Profiler::enable();
+Mage::setIsDeveloperMode(true);
+ini_set('display_errors', 1);
+umask(0);
+Mage::app('admin');
+Mage::register('isSecureArea', 1);
+$rootPath = Mage::getBaseDir();
+set_time_limit(0);
+$sync = new Powerbody_Bridge_Model_Order_Cron();
+$sync->updateOrders();
+exit;
