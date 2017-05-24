50e6e94a7ae1766d8711c6c7bdae2709d1db7eff
michalsobecki <michal.sobecki@paker.fit>
2017-05-24 15:57:21 +0200

    PAK-2757

--- app/code/local/Powerbody/Adminhtml/controllers/Catalog/ProductController.php
+++ app/code/local/Powerbody/Adminhtml/controllers/Catalog/ProductController.php
@@ -2,24 +2,45 @@
 
 class Powerbody_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Controller_Action
 {
+    /**
+     * @var Mage_Adminhtml_Model_Session
+     */
+    private $_session;
+
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_session = Mage::getSingleton('adminhtml/session');
+    }
+
     public function editLimitedAction()
     {
         $this->loadLayout();
         $this->renderLayout();
     }
 
+    /**
+     * @return Mage_Adminhtml_Controller_Action
+     */
     public function saveLimitedAction()
     {
         if (false === $this->_validateFormKey()) {
-            $this->norouteAction();
+            $this->_session->addError($this->__('The form key is invalid, please refresh a page and try again.'));
+            return $this->_redirectReferer();
         }
 
         $requestParamsArray = $this->getRequest()->getParams();
-        $product = Mage::getModel('catalog/product')->load($requestParamsArray['entity_id']);
-        $product->addData($requestParamsArray);
-        $product->save();
+        try {
+            $product = Mage::getModel('catalog/product')->load($requestParamsArray['entity_id']);
+            $product->addData($requestParamsArray);
+            $product->save();
+            $this->_session->addSuccess($this->__('The product has been saved.'));
+        } catch (Exception $e) {
+            Mage::logException($e);
+            $this->_session->addError($e->getMessage());
+        }
 
-        $this->_redirectReferer();
+        return $this->_redirectReferer();
     }
 
     /**
